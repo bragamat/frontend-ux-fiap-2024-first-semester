d4471f5e7b968b4930ff7a0a92c1541801be2ee7
bragamat <mateuscbraga@gmail.com>
2024-02-22 18:52:01 -0300

    feat: adds is loading logic

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,49 +1,41 @@
 import { useEffect, useState } from 'react'
 import { documentToHtmlString } from '@contentful/rich-text-html-renderer'
 import {
-	useParams,
-	Link
+  useParams,
+  Link
 } from 'react-router-dom'
 
 import { Layout } from '../components/Layout'
-import { client } from '../lib/client'
+import { useFetchPostBySlug } from '../hooks/useFetchPostBySlug'
 
 export const Post = () => {
-	const { slug } = useParams()
+  const { slug } = useParams()
 
-	const [post, setPost] = useState(null)
+  const { data: post, isLoading } = useFetchPostBySlug({ slug })
 
-	useEffect(() => {
-		client
-			.getEntries({
-				'fields.postSlug': slug,
-				content_type: 'blogPost',
-			})
-			.then(function(entries) {
-				console.log('post', entries)
-				setPost(entries.items[0] || null)
-			})
-	}, [])
+  return (
+    <Layout>
+      {
+        post ? (
+          <div className="container">
+            <div className="row">
+              <div className="col-12">
+                <h1 className="my-3">{post?.fields?.title}</h1>
+              </div>
 
-	return (
-		<Layout>
-			{post
-				? <div className="container">
-					<div className="row">
-						<div className="col-12">
-							<h1 className="my-3">{post.fields.postTitle}</h1>
-						</div>
+              <div dangerouslySetInnerHTML={{ __html: documentToHtmlString(post.fields.body) }}></div>
 
-						<div dangerouslySetInnerHTML={{ __html: documentToHtmlString(post.fields.postBody) }}></div>
+              <div className="mt-1">
+                <Link to="/" className="btn btn-primary">
+                  Voltar para Home
+                </Link>
+              </div>
+            </div>
+          </div>
+        ) : null
+      }
 
-						<div className="mt-1">
-							<Link to="/" className="btn btn-primary">
-                Voltar para Home
-							</Link>
-						</div>
-					</div>
-				</div>
-				: <div>Carregando...</div>}
-		</Layout>
-	)
+      {isLoading ? (<div>Carregando...</div>) : null}
+    </Layout>
+  )
 }
